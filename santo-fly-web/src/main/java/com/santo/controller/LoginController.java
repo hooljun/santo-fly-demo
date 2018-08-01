f29fb9fc36e50681d61bc612a2f7dd71d5590b47
liangjun.hu <hooljun@163.com>
2018-08-01 16:15:12 +0800

    opt@master#: 错误编码整理

--- santo-fly-web/src/main/java/com/santo/controller/LoginController.java
+++ santo-fly-web/src/main/java/com/santo/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.santo.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.santo.annotation.Log;
 import com.santo.annotation.Pass;
@@ -31,13 +30,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  *  登录接口
